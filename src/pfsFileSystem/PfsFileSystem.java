ed373babb244cdec7ac22d38e448945c1adb2799
ovieu <ovie.u.umukoro@gmail.com>
2017-12-03 13:58:22 -0500

    Add get user input method

--- src/pfsFileSystem/PfsFileSystem.java
+++ src/pfsFileSystem/PfsFileSystem.java
@@ -1,16 +1,43 @@
 package pfsFileSystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 /**
  * The main program
  */
 
-
 public class PfsFileSystem {
     public static void main(String args[]) {
 
-        FsManager fsManger = new FsManager();
+    //  make terminal logic
+        while (true) {
 
-        System.out.println(fsManger.toString());
-        System.out.println(fsManger.toByteData());
+        }
     }
+
+    /**
+     * returns the user input as a string
+     * @return
+     */
+    private static String getUserInput() {
+
+        String inputLine = null;
+
+        try{
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if(inputLine.length() == 0) {
+                return null;
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return inputLine;
+    }
+
+
 }
